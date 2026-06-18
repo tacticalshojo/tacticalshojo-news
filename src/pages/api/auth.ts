25adf9a44ffacb541e7f7724deb63654d6b98dbe
Tacticalshojo <tacticalshojo@gmail.com>
2026-06-18 19:37:53 +0800

    Delete src/pages/api/auth.ts

--- src/pages/api/auth.ts
+++
@@ -1,6 +0,0 @@
-// 💡 關鍵修正：宣告此路徑為動態渲染（Server-side Rendered），打包時直接跳過，避免 req.headers 報錯
-export const prerender = false;
-
-import { auth } from '@openlab/vercel-netlify-cms-github';
-
-export const ALL = auth;
